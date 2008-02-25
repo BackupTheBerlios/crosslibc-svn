edd4f50e0ba0ecd2c96f2b06e189a97b524d35c2
gregorr <gregorr@1e2fc944-ba2a-0410-a2cc-fe5414d8d868>
2008-02-25 04:10:52 +0000

    elfload.exe: Updated.
    test, test.c: Now shows arguments.

--- other/winelf/test.c
+++ other/winelf/test.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char **argv)
 {
+    int i;
     puts("Hello, world!");
+
+    for (i = 0; i < argc; i++) {
+        printf("Arg %d: %s\n", i, argv[i]);
+    }
+
     return 0;
 }
